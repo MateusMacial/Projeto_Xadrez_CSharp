8cae3030011e424b4c3c5355d603eea9492e3382
Mateus Macial <mateus_macial@hotmail.com>
2021-07-02 13:12:50 -0300

    Metodo de imprimir tabuleiro

--- Projeto_Xadrez_CSharp/Projeto_Xadrez_CSharp/Program.cs
+++ Projeto_Xadrez_CSharp/Projeto_Xadrez_CSharp/Program.cs
@@ -7,7 +7,9 @@ namespace Projeto_Xadrez_CSharp
     {
         static void Main(string[] args)
         {
-            Tabuleiro tab = new Tabuleiro(3, 3);
+            Tabuleiro tab = new Tabuleiro(8, 8);
+
+            Tela.imprimirTabuleiro(tab);
 
             Console.ReadLine();
         }
